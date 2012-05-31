fda1e28ed20d25ed1f533ad0f1d2009ccaed8ef6
root <root@cl-t044-070cl.privatedns.com>
2012-05-31 12:59:00 +0200

    live change

---
+++ lib/filter/CardNumbersFormFilter.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * CardNumbers filter form.
+ *
+ * @package    zapnacrm
+ * @subpackage filter
+ * @author     Your name here
+ */
+class CardNumbersFormFilter extends BaseCardNumbersFormFilter
+{
+  public function configure()
+  {
+  }
+}
+
+?>
