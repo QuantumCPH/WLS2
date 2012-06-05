bac4058b0569b310c170a4997150cf75543973f4
Khan Muhammad <khan.muhammad@zerocall.com>
2012-06-05 16:07:02 +0500

    country setting

--- apps/backend/modules/employee/actions/actions.class.php
+++ apps/backend/modules/employee/actions/actions.class.php
@@ -134,7 +134,7 @@ class employeeActions extends sfActions {
         $this->companys = CompanyPeer::doSelect($c);
 
         $pr = new Criteria();
-       $pr->add(ProductPeer::ID, 14);
+       $pr->add(ProductPeer::ID, 4);
         $this->products = ProductPeer::doSelect($pr);
     }
 
