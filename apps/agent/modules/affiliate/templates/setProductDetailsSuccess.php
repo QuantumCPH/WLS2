b153c58ae8b2f77c09128e30eda593e7b4ce7c6d
Rubab Rafique <rr@zerocall.com>
2012-06-08 12:59:33 +0500

    customer registrattion via agent

--- apps/agent/modules/affiliate/templates/setProductDetailsSuccess.php
+++ apps/agent/modules/affiliate/templates/setProductDetailsSuccess.php
@@ -1,6 +1,6 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('Number') ?>
-<?php echo $target.'validateUniqueId'; ?>
+<?php echo sfConfig::get('app_main_url').'validateUniqueId'; ?>
 
 <script type="text/javascript">
 	
